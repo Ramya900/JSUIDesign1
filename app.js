5f2e06f521f2d15ea18d6b63651c77cf972a2848
Ramya900 <ramyaprabhu291@gmail.com>
2020-10-16 14:58:47 +0530

    initial commit on 16-10-2020

---
+++ app.js
@@ -0,0 +1,52 @@
+
+    function inpAddittion(){
+
+        var a,b,c;
+        a=Number(document.getElementById("first").value);
+        b=Number(document.getElementById("second").value);
+        c= a + b;
+        document.getElementById("answer").value= c;
+
+    }
+
+    function inpSubtraction(){
+
+        var a,b,c;
+        a=Number(document.getElementById("first").value);
+        b=Number(document.getElementById("second").value);
+        c= a - b;
+        document.getElementById("answer").value= c;
+
+    }
+
+    function inpMultipliction(){
+
+        var a,b,c;
+        a=Number(document.getElementById("first").value);
+        b=Number(document.getElementById("second").value);
+        c= a * b;
+        document.getElementById("answer").value= c;
+
+    }
+
+    function inpDivision(){
+
+        var a,b,c;
+        a=Number(document.getElementById("first").value);
+        b=Number(document.getElementById("second").value);
+        c= a / b;
+        document.getElementById("answer").value= c;
+
+    }
+
+    function inpModulus(){
+
+        var a,b,c;
+        a=Number(document.getElementById("first").value);
+        b=Number(document.getElementById("second").value);
+        c= a % b;
+        document.getElementById("answer").value= c;
+
+    }
+
+
